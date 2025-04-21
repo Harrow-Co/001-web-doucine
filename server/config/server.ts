9f7ad3eba47935c035bcb2ef07e9be84cf09e57f
Harrow973 <116327514+Harrow973@users.noreply.github.com>
2025-04-21 07:42:45 -0400

    Mise à jour des configurations avec le domaine Railway

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,11 +1,18 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://api.association-doucine.fr'),
+  url: env('PUBLIC_URL', 'https://cms-doucine.up.railway.app'),
   app: {
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  proxy: true,
+  admin: {
+    autoOpen: false,
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
   },
 });
